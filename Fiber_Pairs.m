2c15c40b6f151a3fa7e5377d459fe45f3ed421b0
Tony Fast <tony.fast@gmail.com>
2014-07-23 14:03:44 -0400

    Cumulative Adjancency matrix built for fiber indices, working on visualizations to verify the codes

--- Fiber_Pairs.m
+++ Fiber_Pairs.m
@@ -6,32 +6,79 @@ load INDEX/Indexed_fibers.mat
 %%
 % shift the image to find the neighboring pixels.   there is a function to
 % do it, but it is in a toolbox that cant easily be shared.
+%
+% IMPROVEMENT: Simple BOOLEAN tests only tell if fibers are touching.
+% Change the code to add the number of times the fibers touch.
 
 shifts = [ eye(3); -1 * eye(3) ]
 
+% Remove the edge from the image
 dInoedge = dI( 2:(end-1),2:(end-1),2:(end-1) );
 
 pairs = [0 0];
+
+% Fiber index cumulative adjacency matrix
+G = zeros( max(dI(:)));
 for ii = 1 : size( shifts, 1 )
+    disp(sprintf('Shift #%i of #%i.', ii, size(shifts,1)));
     temp = circshift( dI, shifts(ii,:));
     temp = temp( 2:(end-1),2:(end-1),2:(end-1) );
-    pairs = union( pairs, unique( [dInoedge(:) temp(:)], 'rows' ) ,'rows');
+    pairs = [dInoedge(:) temp(:)];
+    
+    pairs( any(pairs==0,2),:)= [];
+    
+    G(:) = accumarray( ...
+                        sub2ind( ....
+                        max( dI(:) ) * [1 1], ....
+                        pairs(:,2), pairs(:,1)), ...
+                        ones( size(pairs,1), 1), ...
+                        [ numel(G), 1], ...
+                        @sum );
+    
 end
 
-pairs( any(pairs==0,2),:)= []
 
+% The loop accounds for like-index neighbors.  This statement removes that.
+G(:) = G - diag(diag(G));
+
+%% Plot some slices of connected fibers
+
+cut = 300;
+[x,y] = find( G>cut, 3, 'first');
+
+FilterIndex = dI .* ismember( dI, ...
+    [x(:);y(:)] );
+
+trimmed = FilterIndex;
+% Delete zero slices for a more compact viz
+for ii = 1 : 3
+    b = all( trimmed == 0,ii);
+    for jj = setdiff( 1 : 3, ii )
+        deletecol = setdiff( 1 : 3, [ii,jj] );
+        b2 = all( b, jj );
+        for qq = 1 : 3; evalindex{qq} = ':';end
+        evalindex{deletecol} = 'b2';
+        evalexp = sprintf( '%s(%s,%s,%s)= [];','trimmed', ...
+                            evalindex{1}, evalindex{2}, evalindex{3} );
+        
+        eval( evalexp );
+    end
+end
 %%
 
-pairsid = sub2ind( max( dI(:) ) * [1 1], pairs(:,1), pairs(:,2));
-
-G = zeros( max(dI(:)));
-
-G(pairsid) = 1;
-
-%tranpose
-pairsid = sub2ind( max( dI(:) ) * [1 1], pairs(:,2), pairs(:,1));
-
-G(pairsid) = 1;
+clf;
+vol3d( 'Cdata', ...
+    trimmed, ...  
+    'Alpha', trimmed >0 ...
+     );
+ 
+ colormap( rand(10000,3))
+ figure(gcf);
+ axis equal 
+ grid on
+ 
+ % dI .* ismember( dI - volumetric image mapped with salient indices
+ % [x(:);y(:)] - member set of fibers with # of neighbors > ``cut``
 
 %%
 
